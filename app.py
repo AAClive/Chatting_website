0ff0361358422f59e4318880ad9ed747576b7256
clive <98222803+AAClive@users.noreply.github.com>
2022-02-27 23:28:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 from flask import Flask
 from flask import redirect
 from flask import render_template
+
+app=Flask(__name__)
+
+@app.route('/signup')
+def signup():
+  return render_template('signup.html')
