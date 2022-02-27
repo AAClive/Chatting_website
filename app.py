76ea5ac2c0faa3c8345759a07f6ae42c3ca07498
clive <98222803+AAClive@users.noreply.github.com>
2022-02-27 23:31:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import redirect
 from flask import render_template
-
+from flask import request
 app=Flask(__name__)
 
-@app.route('/signup')
+@app.route('/signup',methods=['GET','POST'])
 def signup():
+  if request=="POST": 
+        
   return render_template('signup.html')
 
 if __name__=='__main__':
